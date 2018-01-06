e3181545e11afc35d3a675d32c81d094dab2590f
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2018-01-06 03:32:50 +0000

    Added options to the code for the way the program should work

--- find_library/lib/find_library/cli.rb
+++ find_library/lib/find_library/cli.rb
@@ -4,6 +4,7 @@ class FindLibrary::CLI
     puts "Where's the Library?"
     list_library
     menu
+    goodbye
   end
 
   def list_library
@@ -15,16 +16,26 @@ class FindLibrary::CLI
     DOC
   end
 
-def menu
-  puts "Enter the zipcode of the library you would like to find:"
-  #might try this with the Enter the town of the library... since majority towns have 1 library location.
-  input = gets.strip
-  case input
-  when "10601"
-    puts "Location info for White Plains Public Library"
-  when "10801"
-    puts "Location info for New Rochelle Public Library"
+  def menu
+    puts "Enter the zipcode of the library you would like to find, type list to see the choices again or type exit to quit:"
+    #might try this with the Enter the town of the library... since majority towns have 1 library location.
+    input = nil
+    while input != "exit"
+      input = gets.strip.downcase
+      case input
+      when "10601"
+        puts "Location info for White Plains Public Library"
+      when "10801"
+        puts "Location info for New Rochelle Public Library"
+      when "list" || "LIST"
+        list_library
+      else
+        puts "Not sure which library you want, please type list or exit."
+      end
+    end
   end
-end
 
+  def goodbye
+    puts "Thanks for using Find Library. Happy Reading!"
+  end
 end
