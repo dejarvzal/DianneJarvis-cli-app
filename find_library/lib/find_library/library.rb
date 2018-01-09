56397c4c9c646f469fa43063643d1b14ad012ec6
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2018-01-09 22:55:46 +0000

    add new method to scrap for the libraries

--- find_library/lib/find_library/library.rb
+++ find_library/lib/find_library/library.rb
@@ -3,26 +3,33 @@ class FindLibrary::Library
 
   def self.all
     #return all the libraries in the County
-    # puts <<-DOC.gsub /^\s*/, ''
-    #   10601 - White Plains Public Library
-    #   10801 - New Rochelle Public Library
-    # DOC
+    self.scrape_library
+  end
+#Scrape Westchester Library System and return a library's information based on input
+  def scrape_library
+    libraries = []
 
-    library_1 = self.new
-    library_1.name = "White Plains Public Library"
-    library_1.address = "100 Martine Avenue White Plains, NY 10601"
-    library_1.phone = "914-422-1400"
-    library_1.url = "https://whiteplainslibrary.org/"
-    #library_1.hours = looks like have to go to each individual library page to get the hours of operation
+#Go to wls.org, find library location
+#extract the location information
+#instantiate a library
 
-    library_2 = self.new
-    library_2.name = "New Rochelle Public Library"
-    library_2.address = "One Library Plaza New Rochelle, NY 10801 "
-    library_2.phone = "914-632-7878"
-    library_2.url = "http://nrpl.org/"
-
-    [library_1, library_2]
-
+    libraries
   end
 
 end
+
+# FindLibrary::LibraryScraper.new("")
+# library_1 = self.new
+# library_1.name = "White Plains Public Library"
+# library_1.address = "100 Martine Avenue White Plains, NY 10601"
+# library_1.phone = "914-422-1400"
+# library_1.url = "https://whiteplainslibrary.org/"
+# #library_1.hours = looks like have to go to each individual library page to get the hours of operation
+#
+# library_2 = self.new
+# library_2.name = "New Rochelle Public Library"
+# library_2.address = "One Library Plaza New Rochelle, NY 10801 "
+# library_2.phone = "914-632-7878"
+# library_2.url = "http://nrpl.org/"
+#
+# [library_1, library_2]
