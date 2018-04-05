fbffa5be442a036d5d54112f69243da29dfff38f
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2018-04-05 20:21:54 +0000

    Small edits to cli

--- find_library/lib/find_library/cli.rb
+++ find_library/lib/find_library/cli.rb
@@ -11,20 +11,22 @@ class FindLibrary::CLI
   def list_library #LIST OUT ALL OF THE LIBRARIES by name
     puts "Find the information for a library in Westchester County."
     puts "\n"
-    FindLibrary::Library.all.each_with_index.map do |library, i| library
-     puts "#{i+1}" ". " +"#{library.lines.first}"
-    #  puts "\n"
+    FindLibrary::Library.all.each_with_index.map do |library, i|
+      if "#{library.lines.first}" == nil  #need to get the last 2 libraries to show.
+      puts "#{i+1}" ". " +"#{library.split("\n")[1]}" #their first lines are empty in the array
+      else
+        puts "#{i+1}" ". " +"#{library.lines.first}"
+      end
     end
+    puts "\n"
     puts "Enter the number of the library you would like to find:"
   end
 
   def menu #PROMPT USER TO FIND INFO ON A LIBRARY
     puts "You can type 'list' to see the choices again or type 'exit' to quit:"
     input = nil
-    # binding.pry
     while input != "exit"
       input = gets.strip
-      # if input.split('').all? { |c| (1..44).cover?(c) }
       if (input.to_i >= 1 && input.to_i < 45) && input[/^-?\d+$/] and input.size <= 2
          puts "You selected: " "#{input}" ", which is the "
          puts @library_detail = FindLibrary::Library.all[input.to_i-1]
@@ -32,19 +34,18 @@ class FindLibrary::CLI
          puts "Choose another, type list to see them again or exit:"
       elsif (input.to_i >= 1 && input.to_i < 45) && input[/^-?\d+$/] and input.size <= 2
         @library_detail
-      elsif input == "list"
+      elsif input.downcase == "list"
          list_library
-      elsif input == "exit"
+      elsif input.downcase == "exit"
          goodbye
       else
-         puts "Not sure which library you want. Please type list or exit."
+         puts "Not sure which library you want. Please type the number of the library, 'list' or 'exit'."
       end
     end
   end
 
   def goodbye
     puts "Thanks for using Find Library. Happy Reading!"
-    @library_detail
   end
 end
 #
